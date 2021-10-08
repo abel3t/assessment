fff6931a683ec218fd6dfc3f9f7f9eeb31f1f495
Abel Tran <abeltran.develop@gmail.com>
2021-10-08 17:51:53 +0700

    Update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,11 +25,10 @@ export default function MyApp(props: MyAppProps) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>My page</title>
+        <title>Spiritual Heath Assessment</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <div style={{backgroundColor: "#E2EFE0", minHeight: '100vh'}}>
           <Provider store={store}>
