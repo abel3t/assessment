e78040d3cf55945ff807f08587180fc012f94b10
Abel Tran <abeltran.develop@gmail.com>
2021-10-08 11:03:53 +0700

    Update

--- src/pages/khao-sat.tsx
+++ src/pages/khao-sat.tsx
@@ -41,7 +41,7 @@ const AssessmentPage: React.FC = () => {
   return (
     <div className="p-2 sm:p-3 md:p-4 lg:p-5 flex flex-col justify-center items-center">
       <div className="w-full md:w-3/4 lg:w-2/3 bg-white mb-2 border-gray-400 rounded-lg">
-        <div className="text-3xl text-center text-white rounded-t-lg border-green-100 py-2"
+        <div className="text-2xl md:text-3xl text-center text-white rounded-t-lg border-green-100 py-3"
              style={{ backgroundColor: '#3D8F2E' }}>
           Khảo sát sức khoẻ thuộc linh
         </div>
