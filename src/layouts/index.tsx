2521617dc594865a629e862752911e700d5885c8
Abel Tran <abeltran.develop@gmail.com>
2021-10-12 23:37:50 +0700

    Update

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,8 +5,13 @@ import { CircularProgress } from '@mui/material';
 const Layout: React.FC = ({ children }) => {
   const [ isLoading, setIsLoading ] = useState(false);
 
+	useEffect(() => {
+		setIsLoading(true);	
+	}, []);
+
+
   useEffect(() => {
-    const updateLoadingStatus = () => setIsLoading(!isLoading);
+    const updateLoadingStatus = () => setIsLoading(true);
 
     Router.events.on('routeChangeStart', updateLoadingStatus);
     Router.events.on('routeChangeComplete', updateLoadingStatus);
@@ -19,11 +24,7 @@ const Layout: React.FC = ({ children }) => {
 
   return (
     <>
-      {isLoading ?
-        <div className="flex items-center justify-center w-full" style={{ minHeight: '100vh' }}>
-          <CircularProgress size={70} />
-        </div>
-        : children}
+		  {children}
     </>
   );
 };
